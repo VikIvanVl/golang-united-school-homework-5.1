2d4023fb1ad070f90355f9b7c2e5cd959dc51406
Ivan Victorov <victorov.i.vl@yandex.ru>
2022-05-21 00:52:32 +0300

    Init project

---
+++ square.go
@@ -0,0 +1,25 @@
+package golang_united_school_homework_5_1
+
+type Point struct {
+	x, y int
+}
+
+type Square struct {
+	start Point
+	a     uint
+}
+
+func (squareValue Square) End() Point {
+	var pointValue Point
+	pointValue.x = squareValue.start.x + int(squareValue.a)
+	pointValue.y = squareValue.start.y - int(squareValue.a)
+	return pointValue
+}
+
+func (squareValue Square) Area() uint {
+	return squareValue.a * squareValue.a
+}
+
+func (squareValue Square) Perimeter() uint {
+	return squareValue.a + squareValue.a + squareValue.a + squareValue.a
+}
